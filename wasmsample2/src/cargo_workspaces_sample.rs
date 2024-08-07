32158c422f5972913222e7146735040ac1b1f7ae
0num4 <r.oonuma@matsuri-tech.com>
2024-08-08 00:55:45 +0900

    feat: 動いた

---
+++ wasmsample2/src/cargo_workspaces_sample.rs
@@ -0,0 +1,6 @@
+// extern crate add_one;
+
+// pub fn main() {
+//     let mut a = add_one::add_one(1);
+//     println!("Hello, world! {}", a);
+// }
--- wasmsample2/src/main.rs
+++ wasmsample2/src/main.rs
@@ -1,3 +1,8 @@
+// mod cargo_workspaces_sample;
+extern crate add_one;
+
 fn main() {
     println!("Hello, world!");
+    let mut a = add_one::add_one(1);
+    println!("Hello, world! {}", a);
 }
