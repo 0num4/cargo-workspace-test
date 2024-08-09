8bf5cb2a4b2767ee598cc2d91c7ea19e309bf84e
0num4 <r.oonuma@matsuri-tech.com>
2024-08-10 08:49:31 +0900

    feat: wasmかどうかによってloggerを切り替える

--- learning-wgpu/src/main.rs
+++ learning-wgpu/src/main.rs
@@ -1,7 +1,6 @@
 use add_one::use_variant_sample;
 mod lib;
 pub fn main() {
-    env_logger::init(); // env_logger, useしなくても使えてる:thinking_face:
     lib::run();
     // use_variant_sample(); // extern crateじゃなくてもいいんだ…
 
